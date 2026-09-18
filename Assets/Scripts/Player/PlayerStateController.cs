1c8d78f6e5f1a2a879b16146be75b232496efbbe
Mikael Andreasyan <mikaelandreasyan@gmail.com>
2026-09-17 22:07:05 -0400

    Added radius change based on player state
    - Radius does not exist if the player is stationary
    - Radius has increasing size based on running, standing, crouching

--- Assets/Scripts/Player/PlayerStateController.cs
+++ Assets/Scripts/Player/PlayerStateController.cs
@@ -9,13 +9,16 @@ using UnityEngine.InputSystem;
 public class PlayerStateController : MonoBehaviour
 {
     [SerializeField] private GameObject notebookMenu;
- 
+
     private PlayerControls controls;
     private FirstPersonController firstPersonController;
     private PhotoCameraController photoCameraController;
 
     private bool notebookOpen = false;
     private bool isCrouching = false;
+    private bool isSprinting = false;
+    private bool isMoving = false;
+    private bool hasTakenPhoto = false;
     private bool inPhotoMode = false;
     private bool playerHasControl = true;
 
@@ -53,6 +56,36 @@ public class PlayerStateController : MonoBehaviour
     public void SetCrouching(bool value)
     {
         isCrouching = value;
+    }
+
+    public void SetSprinting(bool value)
+    {
+        isSprinting = value;
+    }
+
+    public bool GetSprinting()
+    {
+        return isSprinting;
+    }
+
+    public void SetMoving(bool value)
+    {
+        isMoving = value;
+    }
+
+    public bool GetMoving()
+    {
+        return isMoving;
+    }
+
+    public void SetTakenPicture(bool value)
+    {
+        hasTakenPhoto = value;
+    }
+
+    public bool HasTakenPicture()
+    {
+        return hasTakenPhoto;
     }
 
     public bool GetPhotoMode()
