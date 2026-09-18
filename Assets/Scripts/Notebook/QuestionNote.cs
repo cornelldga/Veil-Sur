6d2c5066fff3c4a133e2bff425a70fe0b4f2291a
melirom717 <mr2444@cornell.edu>
2026-09-17 23:08:29 -0400

    worked on connecting questionnote to the investigation question/scrapbook JSON

--- Assets/Scripts/Notebook/QuestionNote.cs
+++ Assets/Scripts/Notebook/QuestionNote.cs
@@ -7,10 +7,115 @@ public class QuestionNote : NotebookItem
 {
     [SerializeField] private SolutionRule rule;
 
+    [SerializeField] private TextAsset investigationData;
+    [SerializeField] private int page = 1;
+    [SerializeField] private string questionId;
+    private InvestigationData data;
+    private InvestigationQuestion question;
+
+    public string QuestionText {
+        get {
+            if (question == null) {
+                return "";
+            }
+            return question.questionText;
+        }
+    }
+
+    /// <summary>
+    /// Gets the default feedback for an incorrect answer.
+    /// If question has not beed loaded, returns an empty string.
+    /// </summary>
+    public string DefaultFeedback {
+        get {
+            if (question == null) {
+                return "";
+            }
+            return question.defaultFeedback;
+        }
+    }
+
+    private void Awake() {
+        // When object is created, load question for this QuestionNote from JSON
+        LoadQuestion();
+    }
+
     public SolutionRule Rule() {
         return rule;
     }
 
     // TODO: add question-specific code like changing the question text
     // and validation
+
+    public string Feedback(string subjectId) {
+        // Check subject ids for feedback messages
+        if (question == null || question.answers == null) {
+            return DefaultFeedback;
+        }
+
+        foreach (InvestigationAnswer answer in question.answers) {
+            if (answer.subjectId == subjectId) {
+                return answer.message;
+            }
+        }
+
+        // No matching subject ID, use default feedback
+        return DefaultFeedback;
+    }
+
+    private void LoadQuestion() {
+        if (investigationData == null)
+        {
+            return;
+        }
+        data = JsonUtility.FromJson<InvestigationData>(investigationData.text);
+
+        // Search scrapbook pages
+        foreach (InvestigationPage investigationPage in data.scrapbook) {
+            // Pages that do not match QuestionNote's page number
+            if (investigationPage.page != page || investigationPage.questions == null) {
+                continue;
+            }
+
+            foreach (InvestigationQuestion investigationQuestion in investigationPage.questions) {
+                // If id matches, store question & stop search
+                if (investigationQuestion.questionId == questionId) {
+                    question = investigationQuestion;
+                    return;
+                }
+            }
+        }
+    }
+
+    // Loaded investigation data
+    [System.Serializable]
+    public class InvestigationData {
+        public InvestigationPage[] scrapbook;
+    }
+
+    // One page and questions
+    [System.Serializable]
+    public class InvestigationPage
+    {
+        public int page;
+        public InvestigationQuestion[] questions;
+    }
+
+    // One question and its answers
+    [System.Serializable]
+    public class InvestigationQuestion
+    {
+        public string questionId;
+        public string questionText;
+        public string defaultFeedback;
+        public InvestigationAnswer[] answers;
+    }
+
+    // Answer associated with subject id
+    [System.Serializable]
+    public class InvestigationAnswer {
+        public string subjectId;
+        public bool isCorrect;
+        public string message;
+    }
 }
