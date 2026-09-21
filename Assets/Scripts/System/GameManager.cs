e0f7a9b33a17491e073469a678f114465fc12243
Mikael Andreasyan <mikaelandreasyan@gmail.com>
2026-09-21 18:23:00 -0400

    Got rid of film counter from the code

--- Assets/Scripts/System/GameManager.cs
+++ Assets/Scripts/System/GameManager.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] GameObject canvas;
+    [SerializeField] AudioManager audioManager;
+    [SerializeField] DialogueManager dialogueManager;
     public static GameManager Instance { get; private set; }
-
-    private int filmCount = 0; 
-    [SerializeField] int maxFilmCount = 10; // for playtesting
 
     [Header("Game Settings")]
     [SerializeField] private bool isDebugMode = false;
@@ -30,27 +30,5 @@ public class GameManager : MonoBehaviour
     {
         Debug.Log("GameManager Initialized. Setting up systems...");
         // Setup sound, saving profiles, loading data, etc.
-    }
-    
-    /// <summary>
-    /// Returns the current amount of film the player has.
-    /// </summary>
-    /// <returns> int filmCount : amount of film </returns>
-    public int GetFilmCount()
-    {
-        return filmCount;
-    }
-
-    /// <summary>
-    /// Increments the film counter by one if it is less than the max amount of film
-    /// the player can have.
-    /// </summary>
-    public void IncrementFilmCounter()
-    {
-        if(filmCount < maxFilmCount) 
-        {
-            filmCount++;
-            UIManager.Instance.UpdateFilmCounter();
-        }
     }
 }
